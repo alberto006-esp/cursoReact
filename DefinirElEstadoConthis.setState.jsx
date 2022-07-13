2bb887cca34c61eb7146df6fc3c743028cc236b1
alberto006-esp <albertosuarezolvera@gmail.com>
2022-07-13 20:20:41 +0200

    Definir el estado con this.setState

---
+++ DefinirElEstadoConthis.setState.jsx
@@ -0,0 +1,23 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        name: 'Initial State'
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+      // Cambiamos el estado con setState para modificar la propiedad name asi al darle al boton se cambiara la cadena
+      this.setState({
+        name: 'React Rocks!'
+      });
+    }
+    render() {
+      return (
+        <div>
+          <button onClick={this.handleClick}>Click Me</button>
+          <h1>{this.state.name}</h1>
+        </div>
+      );
+    }
+  };
