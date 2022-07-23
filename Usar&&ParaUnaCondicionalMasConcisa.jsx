b61ce8ddc76571589173cfedb90e5279d617558b
alberto006-esp <albertosuarezolvera@gmail.com>
2022-07-23 22:38:08 +0200

    Usa && para una condicional más concisa

---
+++ Usar&&ParaUnaCondicionalMasConcisa.jsx
@@ -0,0 +1,25 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        display: true
+      }
+      this.toggleDisplay = this.toggleDisplay.bind(this);
+    }
+    toggleDisplay() {
+      this.setState(state => ({
+        display: !state.display
+      }));
+    }
+    render() {
+     
+     
+      return (
+         // Otra forma de poner condiciones sin if/else. usamos el operador && para que la condicion sea concisa.
+         <div>
+           <button onClick={this.toggleDisplay}>Toggle Display</button>
+            {this.state.display && <h1>Displayed!</h1>}
+         </div>
+      );
+    }
+  };
